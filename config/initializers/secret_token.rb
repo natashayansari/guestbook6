89b77517821e62c51eefd85a69dd02ccf387e542
Natasha <natashayansari@students.abtech.edu>
2015-10-02 18:33:57 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook6::Application.config.secret_token = '372826879bb0116c061aaa856453f816fa6ab213ad533df3a620a0d1872172546183c5bc7256a2279a481b3177ecbecb0c7f3eac233ca23a682f3378626540eb'
---
+++ db/migrate/20150930184051_create_students.rb
@@ -0,0 +1,18 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :county
+      t.string :email
+      t.text :description
+      t.boolean :can_send_email
+      t.integer :graduation_year
+      t.float :body_temperature
+      t.decimal :price
+      t.date :birthday
+      t.time :favorite_time
+
+      t.timestamps
+    end
+  end
+end
